1bdd345b4694f100de417a530434c841481731a6
avgolberg <av.golberg@gmail.com>
2022-02-04 15:33:17 +0200

    setup form (3)

--- Setup.xaml.cs
+++ Setup.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace VPN
 {
@@ -27,43 +19,45 @@ namespace VPN
             InitializeComponent();
             pageNumber = 0;
         }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void nextButton_Click(object sender, RoutedEventArgs e)
         {
-            nextButton.IsEnabled = true;
-        }
+            if (pageNumber == 6) this.Close();
 
-        private void RadioButton_Click(object sender, RoutedEventArgs e)
-        {
-            nextButton.IsEnabled = false;
-        }
+            if ((pageNumber == 1 || pageNumber == 2) && browseTextBox.Text.Length == 0) return;
 
-        private void cancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
+            pageNumber++;
+            ShowPage();
 
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            MessageBoxResult result = MessageBox.Show(@"Setup is not complete. If you exit now, the program will not be
-installed.
-
-You may run Setup again at another time to complete the installation.
-
-Exit Setup?", "Exit Setup", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            switch (result)
+            if (pageNumber == 5)
             {
-                case MessageBoxResult.No:
-                    e.Cancel = true;
-                    break;
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.WorkerReportsProgress = true;
+                worker.DoWork += worker_DoWork;
+                worker.ProgressChanged += worker_ProgressChanged;
+
+                worker.RunWorkerAsync();
+            }
+
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            pageNumber--;
+            ShowPage();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(100);
             }
         }
 
-        private void nextButton_Click(object sender, RoutedEventArgs e)
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            pageNumber++;
-            ShowPage();
+            pbStatus.Value = e.ProgressPercentage;
         }
 
         private void ShowPage()
@@ -77,7 +71,6 @@ Exit Setup?", "Exit Setup", MessageBoxButton.YesNo, MessageBoxImage.Question);
 agreement before continuing with the installation.";
                     agreementTextBox.Visibility = Visibility.Visible;
                     radios.Visibility = Visibility.Visible;
-                    nextButton.Content = "Next";
 
                     backButton.Visibility = Visibility.Collapsed;
                     mainInfo.Visibility = Visibility.Collapsed;
@@ -90,10 +83,11 @@ agreement before continuing with the installation.";
                     secondLabel.Text = "Where should VPN Unlimited be installed?";
                     mainInfo.Visibility = Visibility.Visible;
                     mainText.Text = "To continue, click Next. IF you would like to select a different folder, click Browse.";
-                    nextButton.Content = "Next";
                     backButton.Visibility = Visibility.Visible;
                     browse.Visibility = Visibility.Visible;
+                    if (path != null && path.Length != 0) browseTextBox.Text = path;
                     freeSpaceInfo.Visibility = Visibility.Visible;
+                    mainInfoImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/folder_icon.png"));
 
                     agreementTextBox.Visibility = Visibility.Collapsed;
                     radios.Visibility = Visibility.Collapsed;
@@ -103,11 +97,13 @@ agreement before continuing with the installation.";
                     mainLabel.Text = "Select Start Menu Folder";
                     secondLabel.Text = "Where should Setup place the program's shortcuts?";
                     mainInfo.Visibility = Visibility.Visible;
-                    mainInfoImage.Source = new BitmapImage(new Uri(@"Images/folder_icon.png"));
+                    mainInfoImage.Source = new BitmapImage(new Uri("pack://application:,,,/Images/start_folder_icon.ico"));
                     mainText.Text = "To continue, click Next. If you would like to select a different folder, click Browse.";
-                    nextButton.Content = "Next";
                     backButton.Visibility = Visibility.Visible;
                     browse.Visibility = Visibility.Visible;
+                    browseTextBox.Text = "VPN Unlimited";
+                    mainInfoText.Text = "Setup will create the program's shortcuts in the following Start Menu folder.";
+
 
                     freeSpaceInfo.Visibility = Visibility.Collapsed;
                     agreementTextBox.Visibility = Visibility.Collapsed;
@@ -119,28 +115,54 @@ agreement before continuing with the installation.";
                     secondLabel.Text = "Which additional tasks should be performed?";
                     mainText.Text = @"Select the additional tasks you would like Setup to perform while installing VPN
 Unlimited, then click Next.";
-                    agreementTextBox.Visibility = Visibility.Collapsed;
-                    radios.Visibility = Visibility.Collapsed;
                     additionalShortcuts.Visibility = Visibility.Visible;
                     nextButton.Content = "Next";
+
+                    mainInfo.Visibility = Visibility.Collapsed;
+                    browse.Visibility = Visibility.Collapsed;
+                    finalSettings.Visibility = Visibility.Collapsed;
                     break;
                 case 4:
                     mainLabel.Text = "Ready to Install";
                     secondLabel.Text = "Setup is now ready to begin installing VPN Unlimited on your computer.";
-                    mainText.Text = "Click Install to continue with the installation.";
+                    mainText.Text = @"Click Install to continue with the installation, or click Back if you want to review or
+change any settings.";
+                    finalSettings.Visibility = Visibility.Visible;
+                    finalSettings.Text = "Destination location:\n\t" + path + "\n\nStart Menu folder:\n\t" + browseTextBox.Text + "\n\nAdditional tasks:\n\t" + (desktopShortcut ? "Additional shortcuts:\n\t   Create a desktop shortcut" : "-");
                     additionalShortcuts.Visibility = Visibility.Collapsed;
                     nextButton.Content = "Install";
+                    break;
+                case 5:
+                    mainLabel.Text = "Installing";
+                    secondLabel.Text = "Please wait while Setup installs VPN Unlimited on your computer.";
+                    mainText.Text = "Extracting files...";
+                    pbStatus.Visibility = Visibility.Visible;
+
+                    finalSettings.Visibility = Visibility.Collapsed;
+                    backButton.Visibility = Visibility.Collapsed;
+                    nextButton.Visibility = Visibility.Collapsed;
+                    break;
+                case 6:
+                    completeGrid.Visibility = Visibility.Visible;
+                    nextButton.Content = "Finish";
+
+                    rect1.Visibility = Visibility.Hidden;
+                    border1.Visibility = Visibility.Hidden;
+                    pbStatus.Visibility = Visibility.Collapsed;
+                    cancelButton.Visibility = Visibility.Hidden;
+                    logo.Visibility = Visibility.Collapsed;
+                    mainLabel.Visibility = Visibility.Collapsed;
+                    secondLabel.Visibility = Visibility.Collapsed;
+                    mainText.Visibility = Visibility.Collapsed;
+                    nextButton.Visibility = Visibility.Visible;
                     break;
             }
 
         }
-
-        private void backButton_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            pageNumber--;
-            ShowPage();
+            nextButton.IsEnabled = true;
         }
-
         private void desktopShortcutCheckBox_Click(object sender, RoutedEventArgs e)
         {
             if ((bool)desktopShortcutCheckBox.IsChecked) desktopShortcut = true;
@@ -157,5 +179,43 @@ Unlimited, then click Next.";
                 path = openFileDlg.SelectedPath;
             }
         }
+
+        private void pbStatus_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (pbStatus.Value == 100)
+            {
+                pageNumber++;
+                ShowPage();
+            }
+        }
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            nextButton.IsEnabled = false;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (pageNumber != 6)
+            {
+                MessageBoxResult result = MessageBox.Show(@"Setup is not complete. If you exit now, the program will not be
+    installed.
+
+    You may run Setup again at another time to complete the installation.
+
+    Exit Setup?", "Exit Setup", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                switch (result)
+                {
+                    case MessageBoxResult.No:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+        }      
     }
 }
